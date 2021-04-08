552021abe2667fc273f4a6d458d23f9f4302cf9e
SGThr7 <sgthr7@gmail.com>
2021-04-08 19:10:32 +0900

    env: webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+module.exports = {
+	mode: 'development',
+	entry: './src/main.js',
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	resolve: {
+		extensions: ['.js'],
+	},
+}
