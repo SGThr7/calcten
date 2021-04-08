bab6614ca51cb30761918d47c65180d4c2279d5a
SGThr7 <sgthr7@gmail.com>
2021-04-08 19:25:55 +0900

    env: html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
 	mode: 'development',
@@ -18,4 +19,9 @@ module.exports = {
 	resolve: {
 		extensions: ['.ts', '.js'],
 	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: './src/index.html',
+		}),
+	],
 }
