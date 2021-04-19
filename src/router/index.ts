4c6ba79ef7100607700a1acf5d0a3f68829c585a
SGThr7 <sgthr7@gmail.com>
2021-04-19 23:28:46 +0900

    feat: Add title view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import TitleView from '@/components/TitleView.vue'
+import PlayView from '@/components/PlayView.vue'
+
 Vue.use(Router)
 
 export default new Router({
 	routes: [
 		{
+			path: '/',
+			name: 'title-view',
+			component: TitleView,
+		},
+		{
 			path: '/play',
 			name: 'play-view',
-			component: require('@/components/PlayView.vue').default,
+			component: PlayView,
 		},
 		{
 			path: '*',
-			redirect: '/play',
+			redirect: '/',
 		},
 	],
 })
