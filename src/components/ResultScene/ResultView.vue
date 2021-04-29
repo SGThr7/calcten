3135b4bf69d9605623dfb60f01df8bae1f6c3ea5
SGThr7 <sgthr7@gmail.com>
2021-04-29 20:07:45 +0900

    feat: Add result view

---
+++ src/components/ResultScene/ResultView.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="result-view">
+		<div class="title">Result</div>
+		<div class="score">
+			<label for="val-score">Score</label>
+			<span class="value" id="val-score">{{ score }}</span>
+		</div>
+		<menu>
+			<menu-button @click="exit"> Exit </menu-button>
+		</menu>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mapGetters } from 'vuex'
+import MenuButton from '@/components/UIParts/MenuButton.vue'
+
+export default Vue.extend({
+	components: { MenuButton },
+	computed: {
+		...mapGetters('score', ['score']),
+	},
+	methods: {
+		exit() {
+			this.$router.replace('title-view')
+		},
+	},
+})
+</script>
+
+<style lang="sass" scoped>
+.result-view
+	height: 100%
+	width: 100%
+	display: flex
+	flex-direction: column
+	justify-content: space-around
+
+.title
+	font-family: 'Kanit', sans-serif
+	font-size: 3em
+
+label
+	&::after
+		content: ": "
+
+menu
+	padding: 0
+</style>
