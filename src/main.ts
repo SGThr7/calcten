8cff5ef9da6c6de498877b857a1609c096f45eec
SGThr7 <sgthr7@gmail.com>
2021-05-10 19:12:46 +0900

    refactor: Remove vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from '@/App.vue'
 import store from '@/store'
-import router from '@/router'
 
 new Vue({
 	el: '#game',
 	components: { App },
 	template: '<app />',
 	store,
-	router,
 })
