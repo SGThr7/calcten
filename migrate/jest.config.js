056601901a22145399de07c2fdc1dc39bd91c759
SGThr7 <sgthr7@gmail.com>
2021-05-15 16:14:58 +0900

    env: Add ts-jest

--- migrate/jest.config.js
+++ migrate/jest.config.js
@@ -2,5 +2,14 @@ module.exports = {
 	preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
 	transform: {
 		'^.+\\.vue$': 'vue-jest',
+		'^.+\\.tsx?$': 'ts-jest',
+	},
+	globals: {
+		'ts-jest': {
+			tsConfig: 'tsconfig.json',
+			diagnostics: {
+				ignoreCodes: ['TS7006'],
+			},
+		},
 	},
 }
