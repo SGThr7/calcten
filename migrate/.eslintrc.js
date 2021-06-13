b6585c98b8083718353be292e848a0ec57dabe56
SGThr7 <sgthr7@gmail.com>
2021-06-13 19:17:24 +0900

    style: Remove `no-tabs`

--- migrate/.eslintrc.js
+++ migrate/.eslintrc.js
@@ -26,7 +26,6 @@ module.exports = {
 		],
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		'no-tabs': ['error', { allowIndentationTabs: true }],
 		'quote-props': ['error', 'as-needed'],
 		semi: ['error', 'never'],
 	},
